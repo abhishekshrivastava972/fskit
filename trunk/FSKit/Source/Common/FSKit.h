def88ce5ebdf01e31444b94fb63b6dccc01174b2
RedBugz <RedBugz@2fb78232-bf3c-0410-a8a8-396719eec1a4>
2008-10-19 15:11:31 +0000

    try a common FSKit.h for Cocoa and iPhone

--- trunk/FSKit/Source/Common/FSKit.h
+++ trunk/FSKit/Source/Common/FSKit.h
@@ -7,6 +7,12 @@
  *
  */
 
+#if TARGET_OS_IPHONE
+#define NSXMLDocument CXMLDocument 
+#define NSXMLElement CXMLElement 
+#define NSXMLNode CXMLNode
+#endif
+
 #import "FSKConnection.h"
 #import "FSKRequest.h"
 #import "FSKResponse.h"
