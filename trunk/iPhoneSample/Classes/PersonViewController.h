d5a8305157a00244ef1548b3646f5e5308e17b42
RedBugz <RedBugz@2fb78232-bf3c-0410-a8a8-396719eec1a4>
2009-01-28 23:55:59 +0000

    add missing files

---
+++ trunk/iPhoneSample/Classes/PersonViewController.h
@@ -0,0 +1,16 @@
+//
+//  PersonViewController.h
+//  iPhoneSample
+//
+//  Created by Logan Allred on 11/30/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface PersonViewController : UITableViewController {
+
+}
+
+@end
