cd21867a771334d8666e63f4f86862053cc522e3
RedBugz <RedBugz@2fb78232-bf3c-0410-a8a8-396719eec1a4>
2009-03-08 20:17:38 +0000

    start on familytree pedigree requests; fix memory issue in search results

---
+++ trunk/FSKit/Source/Common/FSKRepositoryBase.m
@@ -0,0 +1,14 @@
+//
+//  FSKRepositoryBase.m
+//  FSKit
+//
+//  Created by Logan Allred on 3/3/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "FSKRepositoryBase.h"
+
+
+@implementation FSKRepositoryBase
+
+@end
