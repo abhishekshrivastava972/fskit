b5c46d014665f913424486c82c0b40c33f4e9361
RedBugz <RedBugz@2fb78232-bf3c-0410-a8a8-396719eec1a4>
2010-05-03 04:02:09 +0000

    Copy shared defines to iPhoneSample

---
+++ iPhoneSample/Classes/FSKSharedDefines.h
@@ -0,0 +1,10 @@
+/*
+ *  FSKSharedDefines.h
+ *  FSKit
+ *
+ *  Created by Logan Allred on 4/5/08.
+ *  Copyright 2008 RedBugz Software. All rights reserved.
+ *
+ */
+
+#define kFSK_DEVELOPER_KEY @"NNNN-NNNN-NNNN-NNNN-NNNN-NNNN-NNNN-NNNN"
